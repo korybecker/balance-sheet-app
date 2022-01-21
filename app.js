40278b3682447ad5d328b7893aa711747365e7c4
Kory <korymbecker@gmail.com>
2022-01-20 20:31:28 -0500

    added liabilities routes to app

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const connectDB = require('./db/connect');
-const assets = require('./routes/assets')
-require('dotenv').config();
+const connectDB = require("./db/connect");
+const assets = require("./routes/assets");
+const liabs = require("./routes/liabilities");
+require("dotenv").config();
 
 const app = express();
 
@@ -9,17 +10,17 @@ const app = express();
 app.use(express.json());
 
 // routes
-app.use('/api/v1/assets', assets);
+app.use("/api/v1/assets", assets);
+app.use("/api/v1/liabilities", liabs);
 
 const PORT = 3000 || process.env.PORT;
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(PORT, console.log(`Server listening on port ${PORT}...`));
-    }
-    catch (e) {
-        console.error(e);
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Server listening on port ${PORT}...`));
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 start();
