4eea0ba47207eaa73445554f3962cac3f562d290
Kory <korymbecker@gmail.com>
2022-01-20 20:30:21 -0500

    finished initial assets routes

--- routes/assets.js
+++ routes/assets.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { getAllAssets, createAsset, getSingleAsset } = require('../controllers/assetsController');
-router.route('/').get(getAllAssets).post(createAsset);
-router.route('/:id').get(getSingleAsset);
+const {
+  getAllAssets,
+  createAsset,
+  getSingleAsset,
+  updateAsset,
+  deleteAsset,
+} = require("../controllers/assetsController");
+
+router.route("/").get(getAllAssets).post(createAsset);
+router.route("/:id").get(getSingleAsset).patch(updateAsset).delete(deleteAsset);
 
 module.exports = router;
